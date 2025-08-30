3ba3ad6f1136ec320f872fa3e656956d99f06c4b
Ankit <iankitraj18@gmail.com>
2025-08-30 13:00:37 +0530

    project created

---
+++ 06CurrencyConvertres/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-300 text-black p-4 rounded-xl">Currency Generators!</h1>
+    </>
+  );
+}
+
+export default App;
